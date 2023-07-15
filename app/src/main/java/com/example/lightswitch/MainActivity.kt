3a174681e24be75c10d575615bc530a37e27af83
withercraft303 <lsntao.withercraft303@gmail.com>
2023-07-15 19:27:23 -0400

    on/off button + navbar

--- app/src/main/java/com/example/lightswitch/MainActivity.kt
+++ app/src/main/java/com/example/lightswitch/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.lightswitch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val lightToggle = findViewById<Button>(R.id.LightToggleButton)
+        var lightOn = false
+
+
+        lightToggle.setOnClickListener{
+            // turn on flashlight here
+        }
     }
 }
